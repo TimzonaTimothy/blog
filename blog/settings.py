e1534ad8e5b31389502d3cd3e6e0c80f68a77a3b
TimzonaTimothy <78275394+TimzonaTimothy@users.noreply.github.com>
2021-03-30 16:05:01 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/images')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 #SMTP 'Simple Mail Transfer Protocol' configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
