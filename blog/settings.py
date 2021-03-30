58359c465b8edb153715c4441acd67a9a00469f6
TimzonaTimothy <78275394+TimzonaTimothy@users.noreply.github.com>
2021-03-30 16:01:42 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/images')
 
 #SMTP 'Simple Mail Transfer Protocol' configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
